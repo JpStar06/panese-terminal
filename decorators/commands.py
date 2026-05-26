de0b7a9ace3f5464bf9abbe65f579a0c2f70d3d1
JpStar06 <henriqueeapahel@gmail.com>
2026-05-26 11:37:10 -0300

    reescrita

--- decorators/commands.py
+++ decorators/commands.py
@@ -8,41 +8,27 @@ class CommandRegistry:
             "func": func,
             "help": help_text,
             "details": details
-        }
+            }
 
     def execute(self, name, args, ctx):
-
         cmd = self.commands.get(name)
 
         if not cmd:
             return False
 
-        cmd["func"](
-            args,
-            ctx=ctx,
-            state=ctx.get("state")
-        )
+        cmd["func"](args, ctx=ctx, state=ctx.get("state"))
 
         return True
 
     def get_command(self, name):
         return self.commands.get(name)
 
-
 registry = CommandRegistry()
 
-
 def comando(name, help_text="", details=""):
-
     def decorator(func):
 
-        registry.register(
-            name,
-            func,
-            help_text,
-            details
-        )
+        registry.register(name, func, help_text, details)
 
         return func
-
     return decorator
